67d3076bd65c6cb97853787cabb55a98157749c8
Kamal <dummy@outlook.com>
2025-03-12 18:13:53 +0530

    bug fix

--- provider/test.py
+++ provider/test.py
@@ -12,6 +12,6 @@ jsonm =  [{"content": "Who all starred in avengers 2 movie", "type": "human"}]
 json_payload = json.dumps([msg.dict() for msg in messages])
 model = ChatOpenAI(model="gpt-4o", base_url="", 
                    default_headers={"x-api-key":""})
-rails = LLMRails(config, llm=model)
+rails = LLMRails(config, llm=model, verbose=True)
 print(json_payload)
-print(rails.generate(json.dumps(jsonm)))
+print(rails.generate(json_payload))
