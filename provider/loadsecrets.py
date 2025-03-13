25dfca2dd297f9478f29bbd052e0aecbb24961d8
kamal <kamal@kamal>
2025-03-13 18:46:52 +0000

    bugfix

--- provider/loadsecrets.py
+++ provider/loadsecrets.py
@@ -1,18 +1,14 @@
 import os
 import requests
 import json
+import boto3
+
+# @! create a function to get secret from aws secret manager 
 
 def get_secret(secret_name):
-    
-    headers = {"X-Aws-Parameters-Secrets-Token": os.environ.get('AWS_SESSION_TOKEN')}
-
-    secrets_extension_http_port = os.environ["PARAMETERS_SECRETS_EXTENSION_HTTP_PORT"]
-    secrets_extension_endpoint = f"http://localhost:{secrets_extension_http_port}/secretsmanager/get?secretId={secret_name}"
-  
-    r = requests.get(secrets_extension_endpoint, headers=headers)
-  
-    secret = json.loads(r.text)["SecretString"]
-    return secret
+    client = boto3.client('secretsmanager')
+    response = client.get_secret_value(SecretId=secret_name)
+    return response['SecretString']
 
 # @! create a function which accepts a dictionary of env variable and secret to load, and then sets sets env variable to secrets value using the function defined above
 
