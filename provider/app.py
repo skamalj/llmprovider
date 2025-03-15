d8c5731a996d669db68fe78121885a19e4c11bce
kamal <kamal@kamal>
2025-03-15 08:15:02 +0000

    Nemo and Tools Integrateds.
    code fix  - no base url
    
    fix
    
    bedrock permissions
    
    add logging
    
    azure ai foundary
    
    api reference fixed
    
    circular dependency fixed
    
    log message and azure model fix
    
    bugfix
    
    NEMO TESTED
    
    nemo updated
    
    nemo custom package
    
    debug
    
    debug
    
    fix
    
    tool binding
    
    tool binding
    
    fix
    
    fix
    
    Tool Binding
    
    Update github-actions.yaml
    
    Update requirements.txt
    
    Update requirements.txt
    
    Update requirements.txt
    
    Update requirements.txt
    
    Update requirements.txt
    
    fix
    
    fix

--- provider/app.py
+++ provider/app.py
@@ -1,4 +1,3 @@
-import dill
 import json
 import os
 import traceback
@@ -7,10 +6,16 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_anthropic import ChatAnthropic
 from langchain_aws import ChatBedrockConverse
 from langchain_ollama import ChatOllama
-from nemoguardrails import RailsConfig, LLMRails
+from langchain_azure_ai.chat_models import AzureAIChatCompletionsModel
+from nemoguardrails import RailsConfig
+from langchain_guardrails import NemoRails
 from loadsecrets import load_secrets
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import AIMessage
+from langgraph_utils import create_tools_json, json_to_structured_tools
 
-def get_model(provider: str, model_name: str, messages, base_url: str, **kwargs):
+
+def get_model(provider: str, model_name: str, messages, tools,  **kwargs):
     """
     Creates and returns a pickled model from LangChain for OpenAI, Gemini, Anthropic, AWS Bedrock, Ollama, or Hugging Face.
     
@@ -23,49 +28,63 @@ def get_model(provider: str, model_name: str, messages, base_url: str, **kwargs)
         Pickled model instance
     """
     try:
-        config = RailsConfig.from_path("./config")
+        rails_config = RailsConfig.from_path("./config")
 
         if provider == "openai":
-            model = ChatOpenAI(model_name=model_name, base_url=f"{base_url}/openai/", **kwargs)
+            model = ChatOpenAI(model_name=model_name, **kwargs)
         elif provider == "google":
-            model = ChatGoogleGenerativeAI(model=model_name, base_url=f"{base_url}/google", **kwargs)
+            model = ChatGoogleGenerativeAI(model=model_name, **kwargs)
         elif provider == "anthropic":
-            model = ChatAnthropic(model=model_name, base_url=f"{base_url}/anthropic/", **kwargs)
+            model = ChatAnthropic(model=model_name,**kwargs)
         elif provider == "bedrock":
-            
-            class FlexibleChatBedrockConverse(ChatBedrockConverse):
-                """This model allows extra fields by overriding Config."""
-                class Config:
-                    extra = 'allow'  # Override to allow extra fields
-
-            model = FlexibleChatBedrockConverse(model_id=model_name, base_url=base_url, **kwargs)
+            model = ChatBedrockConverse(model_id=model_name, **kwargs)
         elif provider == "ollama":
-            model = ChatOllama(model=model_name, base_url=base_url, **kwargs)
+            model = ChatOllama(model=model_name, **kwargs)
+        elif provider == "azure":
+            model = AzureAIChatCompletionsModel(
+                endpoint=os.environ["AZURE_INFERENCE_ENDPOINT"],
+                credential=os.environ["AZURE_INFERENCE_CREDENTIAL"],
+                model_name=model_name,
+                api_version=os.environ["AZURE_INFERENCE_API_VERSION"]
+            )
         else:
             raise ValueError(f"Unsupported provider: {provider}")
 
-        rails = LLMRails(config, llm=model)
-        print(messages)
+        
+        if tools:
+            structured_tools = json_to_structured_tools(tools)
+            
+        model_with_tools = model.bind_tools(structured_tools) if tools else model
 
+        # Instantiate NemoRails with configuration and LLM
+        nemorails = NemoRails(config=rails_config, llm=model, generator_llm=model_with_tools, options={"rails": ["input"]})
 
-        return rails.generate(json.dumps(messages))
+        guardrail_chain = nemorails.create_guardrail_chain()
+        prompt = ChatPromptTemplate.from_messages(messages)
+        chain = prompt | guardrail_chain | nemorails.generate_or_exit
+
+        # Invoke the guardrail chain
+        response = [chain.invoke({})]
+        return [json.dumps(AIMessage(msg).dict()) if isinstance(msg, str) else json.dumps(msg.dict()) for msg in response]
 
     except Exception as e:
         error_details = traceback.format_exc()
-        print(f"Error in get_model: {error_details}")  # Log stack trace
+        print(json.dumps({
+            "message": "Error in get_model",
+            "error": str(e),
+            "stack_trace": error_details
+        }))
         raise RuntimeError(f"Failed to generate response: {str(e)}") from e
 
 env_secret_map = {
-    "BASE_APIGW_URL": "LLMFunctionUrlSecret",
-    "APIGW_KEY": "LLMGWApiKeySecret",
     "ANTHROPIC_API_KEY": "AnthropicAPIKey",
     "OPENAI_API_KEY":"OpenAIAPIKey",
-    "GOOGLE_API_KEY": "GeminiAPIKey"
+    "GOOGLE_API_KEY": "GeminiAPIKey",
+    "AZURE_INFERENCE_ENDPOINT": "AzureAIFoundaryEndpoint",
+    "AZURE_INFERENCE_CREDENTIAL": "AzureAIFoundaryAPIKey",
+    "AZURE_INFERENCE_API_VERSION": "AzureAIFoundaryAPIVersion"
 }
 load_secrets(env_secret_map)
-
-base_url = os.environ.get("BASE_APIGW_URL")
-default_headers = {"x-api-key": os.environ.get("APIGW_KEY")}
 
 def lambda_handler(event, context):
     try:
@@ -73,18 +92,17 @@ def lambda_handler(event, context):
         provider = body.get("provider")
         model_name = body.get("model_name")
         messages = body.get("messages")
+        tools = body.get("tools", {})
         additional_params = body.get("params", {})
 
         if not provider or not model_name or not messages:
-            raise ValueError("Both 'provider', 'model_name', and 'messages' are required.")
-        if provider == "anthropic":
-            default_headers["anthropic-api-key"] = os.environ.get("ANTHROPIC_API_KEY","XX")
+            raise ValueError("Params: 'provider', 'model_name', and 'messages' are required.")
 
-        response = get_model(provider, model_name, messages, base_url=base_url, default_headers=default_headers, **additional_params)
+        response = get_model(provider, model_name, messages,tools, **additional_params)
 
         return {
             "statusCode": 200,
-            "body": json.dumps(response)
+            "body": response
         }
     
     except json.JSONDecodeError:
--- provider/test.py
+++ provider/test.py
@@ -1,17 +1,34 @@
 from langchain.schema import HumanMessage, AIMessage
 from nemoguardrails import RailsConfig, LLMRails
 from langchain_openai import ChatOpenAI
+from langchain_azure_ai.chat_models import AzureAIChatCompletionsModel
+from langchain_core.prompts import ChatPromptTemplate
 
-import json
-config = RailsConfig.from_path("./config")
-messages = [
-    HumanMessage(content="How many players play cricket test match per side")
-]
-jsonm =  [{"content": "Who all starred in avengers 2 movie", "type": "human"}]
-# Convert to JSON
-json_payload = json.dumps([msg.dict() for msg in messages])
-model = ChatOpenAI(model="gpt-4o", base_url="", 
-                   default_headers={"x-api-key":""})
-rails = LLMRails(config, llm=model, verbose=True)
-print(json_payload)
-print(rails.generate(json_payload))
+#import json
+#config = RailsConfig.from_path("./config")
+#messages = [
+#    HumanMessage(content="Create a violent story")
+#]
+#jsonm =  [{"content": "I'm sorry, I can't respond to that.", "role": "user"}]
+## Convert to JSON
+#json_payload = [msg.dict() for msg in messages]
+#model = ChatOpenAI(model="gpt-4o")
+#
+##model_azure = AzureAIChatCompletionsModel(
+##                endpoint="",
+##                credential="",
+##                model_name="Phi-4",
+##                api_version="2024-05-01-preview",
+##            )
+#guardrails = process_with_rails(rails_config=config, attributes={"llm":model, "verbose": False}, options={"rails": ["input"]})
+#res = ChatPromptTemplate.from_messages(messages) 
+#chain = res | guardrails
+##rails = LLMRails(config, llm=model)
+##print(json_payload)
+##print(rails.generate(messages=messages, options={
+##    "rails": ["input"]
+##}).response)
+#print(chain.invoke({}))
+
+message = AIMessage(content="I'm sorry, I can't respond to that.")
+print(AIMessage(message.model_dump()))
